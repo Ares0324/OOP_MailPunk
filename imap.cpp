e6b5a2a63d40f1fbda4e416795e2d9ef7ab63b02
Ce Wu <wuce666123@163.com>
2018-12-10 15:25:45 +0000

    1

---
+++ imap.cpp
@@ -0,0 +1,61 @@
+#include "imap.hpp"
+
+
+/*Function in Message class*/
+string Message::getBody(){
+
+}
+
+string Message::getField(){}
+
+void Message::deleteFromMailbox(){}
+
+/*Function in Session class*/
+Message** Session::getMessages(){
+ 
+  set=mailiamp_set_new_interval(1,0);
+  fetch_type=mailiamp_fetch_type_new_fetch_att_list_empty();
+  
+  fetch_att=mailiamp_fetch_att_new_uid();
+  
+  r=mailiamp_fetch_type_new_fetch_att_list_add(fetch_type,fetch_att);
+  check_error(r,"could not add to list");
+
+  r=mailiamp_fetch(imap,set,fetch_type, &fetch_result);
+  check_error(r,"could not get messages");
+
+  for(cur=clist_begin(fetch_result);cur!=NULL;cur=clist_next(cur)){
+    struct mailiamp_msg_att* msg_att;
+    unsigned int uid;//stoped here
+
+    msg_att=clist_content(cur);
+    uid=
+  }
+  
+}
+
+void Session::connect(std::string const& server, size_t port = 143){
+  imap=mailimap_new(0,NULL);//remember to free after logout,returns a new IMAP session
+  r=mailimap_socket_connect(imap, server.c_str(), port);
+  fprintf(stderr, "connect: %i\n",r);
+  check_error(r,"could not connect");
+  
+}
+
+void Session::login(std::string const& userid, std::string const& password){
+  r=mailimap_login(imap, userid.c_str(), password.c_str());
+  check_error(r, "could not login");  
+}
+
+void Session::selectMailbox(std::string const& mailbox){
+  mailiamp_select(imap, mailbox.c_str());
+  check_error(r,"could not select %s\n", mailbox.c_str());
+}
+
+
+Session::Session(){
+  mailiamp_fetch_list_free(fetch_result);
+  mailiamp_set_free(set);
+  mailimap_logout(imap);
+  mailiamp_free(imap);
+}
---
+++ imap.hpp
@@ -0,0 +1,67 @@
+#ifndef IMAP_H
+#define IMAP_H
+#include "imaputils.hpp"
+#include <libetpan/libetpan.h>
+#include <string>
+#include <functional>
+
+namespace IMAP {
+class Message {
+  clistiter* cur;
+  
+public:
+	Message(){}
+	/**
+	 * Get the body of the message. You may chose to either include the headers or not.
+	 */
+	std::string getBody();
+	/**
+	 * Get one of the descriptor fields (subject, from, ...)
+	 */
+	std::string getField(std::string fieldname);
+	/**
+	 * Remove this mail from its mailbox
+	 */
+	void deleteFromMailbox();
+};
+
+class Session {
+  struct mailimap* imap;
+  int r;//for check_error
+
+  struct mailiamp_set* set;
+  struct mailiamp_fetch_type* fetch_stype;
+  struct mailiamp_fetch_att* fetch_att;
+  clist* fecth_result;
+  clist* cur;
+  
+public:
+	Session(std::function<void()> updateUI);
+
+	/**
+	 * Get all messages in the INBOX mailbox terminated by a nullptr (like we did in class)
+	 */
+	Message** getMessages();
+
+	/**
+	 * connect to the specified server (143 is the standard unencrypte imap port)
+	 */
+	void connect(std::string const& server, size_t port = 143);
+
+	/**
+	 * log in to the server (connect first, then log in)
+	 */
+	void login(std::string const& userid, std::string const& password);
+
+	/**
+	 * select a mailbox (only one can be selected at any given time)
+	 * 
+	 * this can only be performed after login
+	 */
+	void selectMailbox(std::string const& mailbox);
+
+	~Session();
+};
+}
+
+#endif /* IMAP_H */
